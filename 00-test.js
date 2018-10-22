c392bd4989a8a3f78e6d1478bf02db68de2eb060
2665214322@qq.com <2665214322@qq.com>
2018-10-22 17:57:40 +0800

    update

--- 00-test.js
+++ 00-test.js
@@ -1,1 +1,11 @@
-console.log(Number.isNaN());
+let info = 2;
+switch (info) {
+    case 1:
+        console.log('1'.repeat(2));
+        break;
+    case 2:
+        console.log('2'.repeat(2));
+    default:
+
+        break;
+}
